c22c77b7daa1dea8f94b9ad29516278f0a7f3a3a
Itay <itaylevy134@gmail.com>
2022-06-28 13:17:24 +0300

    Completed task 3, task 4 is also green

--- src/gallery/ImageFinder.js
+++ src/gallery/ImageFinder.js
@@ -1,15 +1,30 @@
 import { staticImagesData } from "./static-images-db";
+const axios = require("axios");
+const giphyApiKey = "NXNp3Vhfmrv6xJTAP40VoofaPdHSZCj9";
 
 export class ImageFinder {
+  static formatResults = (images) =>
+    images.map(({ id, url, title }) => ({ id, url, title }));
+
   static strategies = {
     static: (query) =>
-      staticImagesData
-        .filter((image) => image.title.indexOf(query) !== -1)
-        .map(({ id, url, title }) => ({ id, url, title }))
+      ImageFinder.formatResults(
+        staticImagesData.filter((image) => image.title.indexOf(query) !== -1)
+      ),
+    giphy: async (query) =>
+      (
+        await axios.get(
+          `https://api.giphy.com/v1/gifs/search?q=${query}&api_key=${giphyApiKey}`
+        )
+      ).data.data.map(({ id, images, title }) => ({
+        id,
+        url: images.original.url,
+        title
+      }))
   };
 
-  search(query, strategyId) {
-    const images = ImageFinder.strategies[strategyId](query);
+  async search(query, strategyId) {
+    const images = await ImageFinder.strategies[strategyId](query);
     return { images, query };
   }
 }
